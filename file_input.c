f6c9b0e7140ac4c6747cde8d5b2b50084e9f2c2b
Matthew Callier <mcallier@gmail.com>
2024-07-05 21:25:39 +0000

    created file_input.c and added db directory to gitignore

---
+++ file_input.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+struct database_header_t{
+    unsigned short version;
+    unsigned short empoyees;
+    unsigned int fileLength;
+};
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 0;
+    }
+    int fd = open(argv[1], O_RDWR | O_CREAT, 0644);
+    if(fd == -1){
+        perror("open");
+        return -1;
+    }
+    char *fileData = "Hot Chocolate!\n";
+    write(fd, fileData, strlen(fileData));
+    close(fd);    
+    return 0;
+}

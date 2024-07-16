348f4b4bb03c6e2b1c8328192306c11c086b84dc
Matthew Callier <mcallier@gmail.com>
2024-07-16 01:24:43 +0000

    rearranged code in double_pointer.c

--- double_pointer.c
+++ double_pointer.c
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    int one = 1;
-    int *pOne = &one; // a pointer to one
-    int **ppOne = &pOne; // a pointer to the pointer to one
+    int one = 1;    
     printf("one: %d\n\n",one); // one
 
+    int *pOne = &one; // a pointer to one
     printf("pOne: %p\n",pOne);  // the address of one
     printf("*pOne: %d\n\n",*pOne); // dereference pOne to get the value of one
 
+    int **ppOne = &pOne; // a pointer to the pointer to one
     printf("ppOne: %p\n",ppOne); // the address of pOne
     printf("*ppOne: %p\n",*ppOne); // dereference ppOne to get the value of pOne
     printf("**ppOne: %d\n\n",**ppOne); // dereference ppOne twice to get the value of one

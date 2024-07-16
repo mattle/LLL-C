b439aec73f383003de470e7c85be4b2c3bf0a5dd
Matthew Callier <mcallier@gmail.com>
2024-07-16 01:21:27 +0000

    improved double_pointer.c

--- double_pointer.c
+++ double_pointer.c
@@ -7,11 +7,13 @@ int main(int argc, char *argv[]){
     int one = 1;
     int *pOne = &one; // a pointer to one
     int **ppOne = &pOne; // a pointer to the pointer to one
-    printf("one: %d\n",one); // one
-    printf("*pOne: %d\n",*pOne); // dereference pOne to get the value of one
+    printf("one: %d\n\n",one); // one
+
     printf("pOne: %p\n",pOne);  // the address of one
-    printf("**pOne: %d\n",**ppOne); // dereference ppOne to get the value of pOne, then dereference pOne to get the value of one
-    printf("*ppOne: %p\n",*ppOne); // dereference ppOne to get the value of pOne
+    printf("*pOne: %d\n\n",*pOne); // dereference pOne to get the value of one
+
     printf("ppOne: %p\n",ppOne); // the address of pOne
+    printf("*ppOne: %p\n",*ppOne); // dereference ppOne to get the value of pOne
+    printf("**ppOne: %d\n\n",**ppOne); // dereference ppOne twice to get the value of one
     return 0;
 }
